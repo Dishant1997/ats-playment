3559adfad37c94d2a89ec69b0a4d80b4eb71eb5f
Dishant1997 <shahdishant28@gmail.com>
2018-11-25 00:36:21 +0530

    Update tornado_server.py

--- ApplicantTracking-master/tornado_server.py
+++ ApplicantTracking-master/tornado_server.py
@@ -25,14 +25,14 @@ class Application(tornado.web.Application):
     handlers = [
 
       # apply stuff
-      (r"/", lib.apply.Process),
-      (r"", lib.apply.Process),
-      (r"/apply", lib.apply.Process),
-      (r"/apply/", lib.apply.Process),
-      (r"/apply/admin", lib.apply.AdminList),
-      (r"/apply/admin/api/tags/([^\/]+)", lib.apply.AdminApiTags),
-      (r"/apply/admin/api/rate/([^\/]+)", lib.apply.AdminApiRate),
-      (r"/apply/admin/api/comment/([^\/]+)", lib.apply.AdminApiComment),
+      #(r"/", lib.apply.Process),
+      #(r"", lib.apply.Process),
+      #(r"/apply", lib.apply.Process),
+      #(r"/apply/", lib.apply.Process),
+      #(r"/apply/admin", lib.apply.AdminList),
+      #(r"/apply/admin/api/tags/([^\/]+)", lib.apply.AdminApiTags),
+      #(r"/apply/admin/api/rate/([^\/]+)", lib.apply.AdminApiRate),
+      #(r"/apply/admin/api/comment/([^\/]+)", lib.apply.AdminApiComment),
 
      ]
      
